4a42f4ae4b651ba9d3597317d1e97cc01851b1b8
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2017-05-15 18:55:31 +0200

    Import files

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+
+alert("Hello world");
+// pop up with an ok/cancel option
+confirm("I love JavaScript!");
+// pop up with ok/cancel and free text option
+prompt("Do you want to learn JavaScript?");
